144ccb1acdd300b1df899d8f1bf51328ef601b49
Porcuupine <>
2025-01-24 11:25:57 -0500

    refactoring

--- 150_problems/two_pointers/test_4_11_container_with_the_most_water.py
+++ 150_problems/two_pointers/test_4_11_container_with_the_most_water.py
@@ -1,4 +1,5 @@
 """same for 75 problems"""
+import pytest
 
 
 class TestContainerWithTheMostWater:
@@ -19,7 +20,20 @@ class TestContainerWithTheMostWater:
     0 <= height[i] <= 104"""
 
     def container_with_the_most_water(self, height: list[int]) -> int:
-        return 0
+        left, right = 0, len(height) - 1
+        maxArea = 0
+        for left in range(len(height)):
+            currArea = min(height[left], height[right]) * (right - left)
+            if currArea > maxArea:
+                maxArea = currArea
 
+        return maxArea
+
+    @pytest.mark.parametrize(
+        "height, expected", [
+            ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+            ([1,1], 1),
+        ]
+    )
     def test_container_with_the_most_water(self, height: list[int], expected):
         assert self.container_with_the_most_water(height) == expected
