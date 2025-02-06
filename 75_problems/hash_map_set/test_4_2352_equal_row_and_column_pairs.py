5a6fbc82d1dc9302e5ecc868e2521a57b4f22c99
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2025-02-06 14:21:30 -0500

    refactoring

--- 75_problems/hash_map_set/test_4_2352_equal_row_and_column_pairs.py
+++ 75_problems/hash_map_set/test_4_2352_equal_row_and_column_pairs.py
@@ -6,25 +6,25 @@ import pytest
 class TestEqualRowAndColumnPairs:
     """
     2352. Equal Row and Column Pairs
-Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
-A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
+    Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
+    A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
 
-Example 1:
-Input: grid = [[3,2,1],[1,7,6],[2,7,7]]
-Output: 1
-Explanation: There is 1 equal row and column pair:
-- (Row 2, Column 1): [2,7,7]
-Example 2:
-Input: grid = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]
-Output: 3
-Explanation: There are 3 equal row and column pairs:
-- (Row 0, Column 0): [3,1,2,2]
-- (Row 2, Column 2): [2,4,2,2]
-- (Row 3, Column 2): [2,4,2,2]
-Constraints:
-n == grid.length == grid[i].length
-1 <= n <= 200
-1 <= grid[i][j] <= 105
+    Example 1:
+    Input: grid = [[3,2,1],[1,7,6],[2,7,7]]
+    Output: 1
+    Explanation: There is 1 equal row and column pair:
+    - (Row 2, Column 1): [2,7,7]
+    Example 2:
+    Input: grid = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]
+    Output: 3
+    Explanation: There are 3 equal row and column pairs:
+    - (Row 0, Column 0): [3,1,2,2]
+    - (Row 2, Column 2): [2,4,2,2]
+    - (Row 3, Column 2): [2,4,2,2]
+    Constraints:
+    n == grid.length == grid[i].length
+    1 <= n <= 200
+    1 <= grid[i][j] <= 105
     """
 
     def equal_row_and_column_pairs(self, grid: list[list[int]]) -> int:
