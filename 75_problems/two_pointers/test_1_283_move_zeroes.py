c0112bee17470d2523aea6a474293b099d13b900
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2025-02-05 21:34:37 -0500

    refactoring

--- 75_problems/two_pointers/test_1_283_move_zeroes.py
+++ 75_problems/two_pointers/test_1_283_move_zeroes.py
@@ -1,31 +1,39 @@
 class TestMoveZeroes:
     """Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
-Note that you must do this in-place without making a copy of the array.
+    Note that you must do this in-place without making a copy of the array.
 
+    Example 1:
+    Input: nums = [0,1,0,3,12]
+    Output: [1,3,12,0,0]
+    Example 2:
+    Input: nums = [0]
+    Output: [0]
+    Constraints:
 
+    1 <= nums.length <= 104
+    -231 <= nums[i] <= 231 - 1
+    """
 
-Example 1:
-
-Input: nums = [0,1,0,3,12]
-Output: [1,3,12,0,0]
-Example 2:
-
-Input: nums = [0]
-Output: [0]
-
-
-Constraints:
-
-1 <= nums.length <= 104
--231 <= nums[i] <= 231 - 1
-"""
     def move_zeroes(self, nums: list[int]) -> None:
+        """
+        [0,1,0,3,12]
+         s f     change and f++, s++
+        [1,0,0,3,12]
+           s f   f++ ? nums[s] != 0 ? s++: ;
+           s   f    change
+        [1,3,0,0,12]
+           s   f      f++   nums[s] != 0 ? s++ : ;
+             s    f   change
+        [1,3,12,0,0]
+        """
         slow = 0
-        for fast in range(len(nums)):
+        fast = 0
+        while fast < len(nums):
             if nums[fast] != 0 and nums[slow] == 0:
-                nums[slow], nums[fast] = nums[fast], nums[slow]
+                nums[fast], nums[slow] = nums[slow], nums[fast]
 
+            fast += 1
             if nums[slow] != 0:
                 slow += 1
 
--- 75_problems/two_pointers/test_3_11_container_with_the_most_water.py
+++ 75_problems/two_pointers/test_3_11_container_with_the_most_water.py
@@ -3,48 +3,54 @@ import pytest
 
 class TestContainerWithTheMostWater:
     """You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
-Find two lines that together with the x-axis form a container, such that the container contains the most water.
-Return the maximum amount of water a container can store.
-Notice that you may not slant the container.
+    Find two lines that together with the x-axis form a container, such that the container contains the most water.
+    Return the maximum amount of water a container can store.
+    Notice that you may not slant the container.
 
+    Example 1:
+    Input: height = [1,8,6,2,5,4,8,3,7]
+    Output: 49
+    Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+    Example 2:
+    Input: height = [1,1]
+    Output: 1
+    Constraints:
+    n == height.length
+    2 <= n <= 105
+    0 <= height[i] <= 104
+    """
 
-Example 1:
-Input: height = [1,8,6,2,5,4,8,3,7]
-Output: 49
-Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
-Example 2:
-Input: height = [1,1]
-Output: 1
-
-Constraints:
-
-n == height.length
-2 <= n <= 105
-0 <= height[i] <= 104
-"""
     def container_with_the_most_water(self, height: list[int]) -> int:
-        # return 49
-        # currValue = maxValue = 0
-        left = 0
-        right = len(height) - 1
-        maxValue = 0
+        """
+        [1,8,6,2,5,4,8,3,7]
+         l               r  res = min(height[l], height[r]) * (r - l) = min(1,7)*(9-1) = 8
+           l             r  res = min(8,7)*(9-2)=49
+           l           r    res = 3*6=18
+           l         r      res = 8*5=40
+             l       r      res = 6*4=24
+               l     r      res = 2*3=6
+                 l   r      res = 5*2=10
+                   l r      res = 4*1=4
+        """
+        currArea = maxArea = 0
+        left, right = 0, len(height) - 1
         while left < right:
-            currValue = min(height[right], height[left]) * (right - left)
-            maxValue = max(maxValue, currValue)
+            currArea = min(height[left], height[right]) * (right - left)
+            if currArea > maxArea:
+                maxArea = currArea
 
-            if height[left] < height[right]:
-                left += 1
-            else:
+            if height[left] > height[right]:
                 right -= 1
+            else:
+                left += 1
 
-        return maxValue
-
-
+        return maxArea
 
     @pytest.mark.parametrize(
         'height, expected',
         [
             ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+            ([1, 1], 1),
         ])
     def test_container_with_the_most_water(self, height, expected):
         assert self.container_with_the_most_water(height) == expected
