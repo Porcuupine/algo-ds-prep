f22aafdf76b8c7f1692565d33bde2cad04caf90d
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2025-02-06 11:42:21 -0500

    refactoring

--- 75_problems/sliding_window/test_2_1456_maximum_number_of_vowels_in_a_subaray.py
+++ 75_problems/sliding_window/test_2_1456_maximum_number_of_vowels_in_a_subaray.py
@@ -3,42 +3,45 @@ import pytest
 
 class TestMaxNumberOfVowels:
     """1456. Maximum Number of Vowels in a Substring of Given Length
-Medium
+    Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
 
-Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+    Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+    Example 1:
+    Input: s = "abciiidef", k = 3
+    Output: 3
+    Explanation: The substring "iii" contains 3 vowel letters.
+    Example 2:
+    Input: s = "aeiou", k = 2
+    Output: 2
+    Explanation: Any substring of length 2 contains 2 vowels.
+    Example 3:
+    Input: s = "leetcode", k = 3
+    Output: 2
+    Explanation: "lee", "eet" and "ode" contain 2 vowels.
+    Constraints:
+    1 <= s.length <= 105
+    s consists of lowercase English letters.
+    1 <= k <= s.length"""
 
-Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
-
-Example 1:
-
-Input: s = "abciiidef", k = 3
-Output: 3
-Explanation: The substring "iii" contains 3 vowel letters.
-Example 2:
-
-Input: s = "aeiou", k = 2
-Output: 2
-Explanation: Any substring of length 2 contains 2 vowels.
-Example 3:
-
-Input: s = "leetcode", k = 3
-Output: 2
-Explanation: "lee", "eet" and "ode" contain 2 vowels.
-
-Constraints:
-
-1 <= s.length <= 105
-s consists of lowercase English letters.
-1 <= k <= s.length"""
     def max_number_of_vowels(self, s: str, k: int):
-        vowels = ('a', 'e', 'i', 'o', 'u')
-        currNumber = maxNumber = sum([1 for x in s[:k] if x in vowels])
+        """
+        "abciiidef" k = 3
+         l r    count = 1 // because just 1 vowel
+          l r   count = 1 // because "i" was added and "a" was deleted
+           l r  count = 2 // "i" was added
+            l r count = 3 // "i" was added
+             l r  count = 2 //"i" was deleted
+              l r  count = 2 // "e" was added and "i" was deleted
+               l r  count = 1 // "i" was deleted
+        """
+        vowels = ("a", "e", "i", "o", "u")
+        currSum = maxSum = sum([1 for x in s[:k] if x in vowels])
         for i in range(k, len(s)):
-            currNumber += (s[i] in vowels) - (s[i - k] in vowels)
-            if maxNumber < currNumber:
-                maxNumber = currNumber
+            currSum += (s[i] in vowels) - (s[i - k] in vowels)
+            if currSum > maxSum:
+                maxSum = currSum
 
-        return maxNumber
+        return maxSum
 
     @pytest.mark.parametrize(
         's, k, expected', [
--- 75_problems/sliding_window/test_3_1004_max_consecutive_ones.py
+++ 75_problems/sliding_window/test_3_1004_max_consecutive_ones.py
@@ -3,41 +3,48 @@ import pytest
 
 class TestMaxConsecutiveOnes:
     """1004. Max Consecutive Ones III
-Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+    Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
 
-Example 1:
+    Example 1:
+    Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+    Output: 6
+    Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+    Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+    Example 2:
+    Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
+    Output: 10
+    Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
+    Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+    Constraints:
+    1 <= nums.length <= 105
+    nums[i] is either 0 or 1.
+    0 <= k <= nums.length"""
 
-Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
-Output: 6
-Explanation: [1,1,1,0,0,1,1,1,1,1,1]
-Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
-Example 2:
-
-Input: nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], k = 3
-Output: 10
-Explanation: [0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,1,1,1,1]
-Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
-
-Constraints:
-1 <= nums.length <= 105
-nums[i] is either 0 or 1.
-0 <= k <= nums.length"""
     def max_consecutive_ones(self, nums, k):
+        """
+        [1,1,1,0,0,0,1,1,1,1,0]   k = 2
+         l         r              k = -1:  currNum = 6 - 1
+           l       r
+             l     r
+               l   r
+                 l   r            k = 0    currNum = 3 - 1
+                 l           r    k = -1   currNum = 7 - 1
+        """
         left = 0
-        for i in range(len(nums)):
-            if nums[i] == 0:
+        for right in range(len(nums)):
+            if nums[right] == 0:
                 k -= 1
             if k < 0:
                 if nums[left] == 0:
                     k += 1
                 left += 1
 
-        return i - left + 1
+        return right - left + 1
 
     @pytest.mark.parametrize(
         'nums, k, expected', [
-            # ([1,1,1,0,0,0,1,1,1,1,0], 2, 6),
-            ([1, 0, 0, 0, 1, 0, 1], 2, 4)
+            ([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2, 6),
+            # ([1, 0, 0, 0, 1, 0, 1], 2, 4)
         ]
     )
     def test_max_consecutive_ones(self, nums, k, expected):
