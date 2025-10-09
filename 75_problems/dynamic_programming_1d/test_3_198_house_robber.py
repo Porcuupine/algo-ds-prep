b4d226ffdefded0fb62400a24e11ae7b370ede8e
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2025-10-08 23:05:15 -0400

    dp house

---
+++ 75_problems/dynamic_programming_1d/test_3_198_house_robber.py
@@ -0,0 +1,50 @@
+import pytest
+
+
+class TestHouseRobber:
+    """
+    You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+    Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+    Example 1:
+    Input: nums = [1,2,3,1]
+    Output: 4
+    Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+    Total amount you can rob = 1 + 3 = 4.
+    Example 2:
+    Input: nums = [2,7,9,3,1]
+    Output: 12
+    Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+    Total amount you can rob = 2 + 9 + 1 = 12.
+    Constraints:
+    1 <= nums.length <= 100
+    0 <= nums[i] <= 400
+    """
+
+    def house_robber(self, nums: list[int]) -> int:
+
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        # n = len(nums)
+        # dp = [0] * n
+        # dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        #
+        # for i in range(2, n):
+        #     dp[i] = nums[i] + max(dp[i - 1], dp[i - 2])
+        #
+        # return dp[-1]
+
+        prev2, prev1 = 0, 0  # dp[i-2], dp[i-1]
+        for n in nums:
+            prev2, prev1 = prev1, max(prev1, prev2 + n)
+        return prev1
+
+    @pytest.mark.parametrize("cost, expected", [
+        ([1, 2, 3, 1], 4),
+        ( [2,7,9,3,1], 12),
+    ])
+    def test_climbing_stairs_cost(self, nums, expected):
+        assert self.house_robber(nums) == expected
