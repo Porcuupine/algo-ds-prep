d7807d5fa51471dcc3038dd376a8dec8b0c161b4
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2025-11-23 21:44:52 -0500

    2p

--- 150_problems/array_strings/test_16_42_trapping_water.py
+++ 150_problems/array_strings/test_16_42_trapping_water.py
@@ -18,24 +18,46 @@ class TestTrappingWater:
     """
 
     def trap(self, height: list[int]) -> int:
-        n = len(height)
-        if n == 0:
-            return 0
+        # 2 pass O(n) time O(n) space
+        # n = len(height)
+        # if n == 0:
+        #     return 0
+        #
+        # left_max = [0] * n
+        # right_max = [0] * n
+        #
+        # left_max[0] = height[0]
+        # for i in range(1, n):
+        #     left_max[i] = max(left_max[i - 1], height[i])
+        #
+        # right_max[n - 1] = height[n - 1]
+        # for i in range(n - 2, -1, -1):
+        #     right_max[i] = max(right_max[i + 1], height[i])
+        #
+        # water = 0
+        # for i in range(n):
+        #     water += max(0, min(left_max[i], right_max[i]) - height[i])
+        #
+        # return water
 
-        left_max = [0] * n
-        right_max = [0] * n
-
-        left_max[0] = height[0]
-        for i in range(1, n):
-            left_max[i] = max(left_max[i - 1], height[i])
-
-        right_max[n - 1] = height[n - 1]
-        for i in range(n - 2, -1, -1):
-            right_max[i] = max(right_max[i + 1], height[i])
-
+        # 2 pointers O(n) time O(1) space
+        left, right = 0, len(height) - 1
+        left_max = right_max = 0
         water = 0
-        for i in range(n):
-            water += max(0, min(left_max[i], right_max[i]) - height[i])
+
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    water += left_max - height[left]
+                left += 1
+            else:
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    water += right_max - height[right]
+                right -= 1
 
         return water
 
