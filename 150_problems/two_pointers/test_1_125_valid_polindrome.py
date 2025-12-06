891e331529822c4dfeca18149bbdd6ca37bf5313
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2025-12-06 11:52:32 -0500

    two

--- 150_problems/two_pointers/test_1_125_valid_polindrome.py
+++ 150_problems/two_pointers/test_1_125_valid_polindrome.py
@@ -26,9 +26,29 @@ class TestVilidPalindrome:
 
     1 <= s.length <= 2 * 105
     s consists only of printable ASCII characters."""
-    def valid_polindrome(self, s:str):
-        cleaned = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
-        return cleaned == cleaned[::-1]
+
+    def valid_polindrome(self, s: str):
+        # O(n) time O(n) space
+        # cleaned = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        # return cleaned == cleaned[::-1]
+        # two pointers O(n) O(1) space:
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            # skip non-alphanumeric
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            # compare
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
 
     @pytest.mark.parametrize(
         "s, expected", [
@@ -39,4 +59,3 @@ class TestVilidPalindrome:
     )
     def test_valid_polindrome(self, s, expected):
         assert self.valid_polindrome(s) == expected
-
