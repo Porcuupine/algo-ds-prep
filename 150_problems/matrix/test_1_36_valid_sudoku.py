1ed37a5f8f0f4ec3be03928f77a35eb884820706
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2025-12-23 14:10:53 -0500

    matrix

--- 150_problems/matrix/test_1_36_valid_sudoku.py
+++ 150_problems/matrix/test_1_36_valid_sudoku.py
@@ -41,5 +41,36 @@ class TestValidSudoku:
     board[i][j] is a di®git 1-9 or '.'.
     """
 
-    def check_sudoku(self, board: list[list[str]]):
-        return False
+    def validate_sudoku(self, board: list[list[str]]):
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == ".":
+                    continue
+                box_index = (r // 3) * 3 + (c // 3)
+                if val in rows[r] or val in cols[c] or val in boxes[box_index]:
+                    return False
+
+                rows[r].add(val)
+                cols[c].add(val)
+                boxes[box_index].add(val)
+
+        return True
+
+    @pytest.mark.parametrize("board, expected", [
+        ([["5", "3", ".", ".", "7", ".", ".", ".", "."]
+             , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+             , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+             , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+             , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+             , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+             , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+             , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+             , [".", ".", ".", ".", "8", ".", ".", "7", "9"]], True),
+    ])
+    def test_validate_sudoku(self, board, expected):
+        assert self.validate_sudoku(board) == expected
