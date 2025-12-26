9e40cafe901b5ffecd5a428cde896c9bd069df1a
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2025-12-26 17:32:40 -0500

    ref

---
+++ 150_problems/matrix/test_2_54_spiral_matrix.py
@@ -0,0 +1,56 @@
+import pytest
+
+
+class TestSpiralMatrix:
+    """
+    Given an m x n matrix, return all elements of the matrix in spiral order.
+    Example 1:
+    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    Output: [1,2,3,6,9,8,7,4,5]
+    Example 2:
+    Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+    Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+    Constraints:
+    m == matrix.length
+    n == matrix[i].length
+    1 <= m, n <= 10
+    -100 <= matrix[i][j] <= 100
+    """
+
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        if not matrix:
+            return []
+        res = []
+        top, bottom = 0, len(matrix) - 1
+        left, right = 0, len(matrix[0]) - 1
+
+        while top <= bottom and left <= right:
+            # 1. top row
+            for c in range(left, right + 1):
+                res.append(matrix[top][c])
+            top += 1
+
+            # 2. right column
+            for r in range(top, bottom + 1):
+                res.append(matrix[r][right])
+            right -= 1
+
+
+            if top <= bottom:
+                # 3. bottom row
+                for c in range(right, left - 1, -1):
+                    res.append(matrix[bottom][c])
+                bottom -= 1
+
+            if left <= right:
+                # 4. left column
+                for r in range(bottom, top - 1, -1):
+                    res.append(matrix[r][left])
+                left += 1
+        return res
+
+    @pytest.mark.parametrize("matrix, expected", [
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
+    ])
+    def test_spiralOrder(self, matrix, expected):
+        assert self.spiralOrder(matrix) == expected
