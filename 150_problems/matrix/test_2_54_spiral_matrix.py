98e82e2d786b1032d2246e30096d0a61da39129f
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2025-12-27 11:36:42 -0500

    ref

--- 150_problems/matrix/test_2_54_spiral_matrix.py
+++ 150_problems/matrix/test_2_54_spiral_matrix.py
@@ -25,32 +25,33 @@ class TestSpiralMatrix:
         left, right = 0, len(matrix[0]) - 1
 
         while top <= bottom and left <= right:
-            # 1. top row
+            # top row:
             for c in range(left, right + 1):
                 res.append(matrix[top][c])
             top += 1
 
-            # 2. right column
+            # right column:
             for r in range(top, bottom + 1):
                 res.append(matrix[r][right])
             right -= 1
 
-
             if top <= bottom:
-                # 3. bottom row
+                # bottom row (reversed)
                 for c in range(right, left - 1, -1):
                     res.append(matrix[bottom][c])
                 bottom -= 1
 
             if left <= right:
-                # 4. left column
+                # left column:
                 for r in range(bottom, top - 1, -1):
                     res.append(matrix[r][left])
                 left += 1
+
         return res
 
     @pytest.mark.parametrize("matrix, expected", [
         ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
+        ([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7])
     ])
     def test_spiralOrder(self, matrix, expected):
         assert self.spiralOrder(matrix) == expected
