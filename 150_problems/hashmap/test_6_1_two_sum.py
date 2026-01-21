2335a978646ed3271da686a1a78d5c6db9329ddd
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2026-01-20 19:48:26 -0500

    tests

--- 150_problems/hashmap/test_6_1_two_sum.py
+++ 150_problems/hashmap/test_6_1_two_sum.py
@@ -28,6 +28,8 @@ class TestTwoSum:
 
     @pytest.mark.parametrize("nums, target, expected", [
         ([2, 7, 11, 15], 9, [0, 1]),
+        ([3, 2, 4], 6, [1, 2]),
+        ([3, 3], 6, [0, 1]),
     ])
     def test_two_sum(self, nums, target, expected):
         assert self.two_sum(nums, target)
