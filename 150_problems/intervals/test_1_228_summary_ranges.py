30e702747fbcb44e0632ef3db001bbf3716cd9a3
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2026-01-29 21:50:41 -0500

    ref

--- 150_problems/intervals/test_1_228_summary_ranges.py
+++ 150_problems/intervals/test_1_228_summary_ranges.py
@@ -56,6 +56,7 @@ class TestSummaryRanges:
 
     @pytest.mark.parametrize("nums, expected", [
         ([0, 1, 2, 4, 5, 7], ["0->2", "4->5", "7"]),
+        ([0, 2, 3, 4, 6, 8, 9], ["0", "2->4", "6", "8->9"])
     ])
     def test_summary_range(self, nums, expected):
         assert self.summary_range(nums) == expected
