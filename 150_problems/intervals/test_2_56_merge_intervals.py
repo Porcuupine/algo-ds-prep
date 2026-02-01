7eecab08ab18d1610081ed2ece718c7e2bb22477
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2026-01-31 20:56:37 -0500

    ref

--- 150_problems/intervals/test_2_56_merge_intervals.py
+++ 150_problems/intervals/test_2_56_merge_intervals.py
@@ -42,3 +42,10 @@ class TestMergeIntervals:
                 merged.append([start, end])
 
         return merged
+
+    @pytest.mark.parametrize("intervals, expected", [
+        ([[1, 3], [2, 6], [8, 10], [15, 18]], [[1, 6], [8, 10], [15, 18]]),
+        ([[1, 4], [4, 5]], [[1, 5]]),
+    ])
+    def test_merge(self, intervals, expected):
+        assert self.merge(intervals) == expected
