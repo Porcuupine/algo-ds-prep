f491b5ceaa05fcefa3dac2814b7d0ee413deb709
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2026-02-11 18:55:41 -0500

    ref

--- 150_problems/linked_list/test_3_57_merge_two_sorted_list.py
+++ 150_problems/linked_list/test_3_57_merge_two_sorted_list.py
@@ -7,16 +7,18 @@ class ListNode:
         self.next = next
 
 
-def build_linked_list(nums):
+def build_linked_list(nums: list[int]) -> ListNode:
     dummy = ListNode(0)
     curr = dummy
+
     for n in nums:
         curr.next = ListNode(n)
         curr = curr.next
+
     return dummy.next
 
 
-def linked_list_to_array(head):
+def linked_list_to_array(head: ListNode) -> list[int]:
     result = []
     while head:
         result.append(head.val)
@@ -24,7 +26,7 @@ def linked_list_to_array(head):
     return result
 
 
-class TestMergeTwoSortedList:
+class Solution:
     """
     You are given the heads of two sorted linked lists list1 and list2.
     Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
@@ -44,28 +46,31 @@ class TestMergeTwoSortedList:
     Both list1 and list2 are sorted in non-decreasing order.
     """
 
-    def merge_two_sorted_list(self, list1: ListNode | None, list2: ListNode | None) -> list[int]:
-        dummy = ListNode()
-        tail = dummy
-
+    def merge_two_sorted_lists(self, list1: ListNode | None, list2: ListNode | None) -> ListNode:
+        dummy = ListNode(0)
+        curr = dummy
         while list1 and list2:
             if list1.val < list2.val:
-                tail.next = list1
+                curr.next = list1
                 list1 = list1.next
             else:
-                tail.next = list2
+                curr.next = list2
                 list2 = list2.next
 
-            tail = tail.next
+            curr = curr.next
 
-        tail.next = list1 if list1 else list2
+        curr.next = list1 if list1 else list2
+
         return dummy.next
 
-    @pytest.mark.parametrize("list1, list2, expected", [
-        ([1, 2, 4], [1, 3, 4], [1, 1, 2, 3, 4, 4]),
-    ])
-    def test_merge_two_sorted_list(self, list1, list2, expected):
-        head1 = build_linked_list(list1)
-        head2 = build_linked_list(list2)
-        result = self.merge_two_sorted_list(head1, head2)
-        assert linked_list_to_array(result) == expected
+
+@pytest.mark.parametrize("list1, list2, expected", [
+    ([1, 2, 4], [1, 3, 4], [1, 1, 2, 3, 4, 4]),
+    ([], [], []),
+    ([], [0], [0]),
+])
+def test_merge_two_sorted_list(list1, list2, expected):
+    head1 = build_linked_list(list1)
+    head2 = build_linked_list(list2)
+    result = Solution().merge_two_sorted_lists(head1, head2)
+    assert linked_list_to_array(result) == expected
