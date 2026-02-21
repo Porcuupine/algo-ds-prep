75bc6ecd690844b38202b8b7d5a1d46d319ac777
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2026-02-20 21:41:36 -0500

    ref

--- 150_problems/linked_list/test_5_92_reversed_linked_list_II.py
+++ 150_problems/linked_list/test_5_92_reversed_linked_list_II.py
@@ -2,18 +2,18 @@ import pytest
 
 
 class ListNode:
-    def __init__(self, val=0, next=next):
+    def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
 
-def build_linked_list(nums: list[int]):
+def build_linked_list(nums: list[int]) -> ListNode:
     dummy = ListNode()
     curr = dummy
     for n in nums:
         curr.next = ListNode(n)
         curr = curr.next
-    return dummy
+    return dummy.next
 
 
 def linked_list_to_list(head: ListNode | None):
@@ -40,23 +40,39 @@ class Solution:
     Follow up: Could you do it in one pass?"""
 
     def reverse_linked_list(self, head: ListNode | None, left: int, right: int):
-        while left < right:
-            head[left], head[right] = head[right], head[left]
+        if not head or left == right:
+            return head
 
-            left += 1
-            right -= 1
+        dummy = ListNode(0, head)
+        prev = dummy
 
+        # move prev to node before left:
+        for _ in range(left - 1):
+            prev = prev.next
 
-        return head
+        # sublist start
+        curr = prev.next
 
+        # reverse the sublist normally
+        prev_sub = None
+        for _ in range(right - left + 1):
+            nxt = curr.next
+            curr.next = prev_sub
+            prev_sub = curr
+            curr = nxt
 
+            # reconnect
+        prev.next.next = curr
+        prev.next = prev_sub
+
+        return dummy.next
 
 
 @pytest.mark.parametrize("values, left, right, expected", [
-    ([1, 2, 3, 4, 5], 2, 4),
-    ([5], 1, 1),
+    ([1, 2, 3, 4, 5], 2, 4, [1, 4, 3, 2, 5]),
+    # ([5], 1, 1, [5]),
 ])
 def test_reverse_linked_list(values, left, right, expected):
     head = build_linked_list(values)
-    result = Solution().reverse_linked_list(head)
+    result = Solution().reverse_linked_list(head, left, right)
     assert linked_list_to_list(result) == expected
