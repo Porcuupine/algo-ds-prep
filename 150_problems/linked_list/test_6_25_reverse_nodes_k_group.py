66ac444d6d4bef01010d72656eece0f8229743d2
Porcuupine <181142963+Porcuupine@users.noreply.github.com>
2026-02-22 08:42:16 -0500

    ref

--- 150_problems/linked_list/test_6_25_reverse_nodes_k_group.py
+++ 150_problems/linked_list/test_6_25_reverse_nodes_k_group.py
@@ -42,36 +42,40 @@ class Solution:
     """
 
     def reverse_nodes(self, head: ListNode | None, k: int) -> ListNode:
-        dummy = ListNode()
-        dummy.next = head
-        group_prev = dummy
+        dummy_head = ListNode()
+        dummy_head.next = head
+
+        prev_group_tail = dummy_head
 
         while True:
-            # find kth node
-            kth = group_prev
+            # find the end of the current k-group
+            group_end = prev_group_tail
             for _ in range(k):
-                kth = kth.next
-                if not kth:
-                    return dummy.next  # less than k nodes left
+                group_end = group_end.next
+                if not group_end:
+                    return dummy_head.next  # not enough nodes left
 
-            group_next = kth.next
+            next_group_head = group_end.next
 
-            # reverse k nodes
-            prev, curr = group_next, group_prev.next
+            # reverse this group
+            node = prev_group_tail.next  # first node in group
+            reversed_head = next_group_head  # this will become the tail connector
+
             for _ in range(k):
-                tmp = curr.next
-                curr.next = prev
-                prev = curr
-                curr = tmp
+                next_node = node.next
+                node.next = reversed_head
+                reversed_head = node
+                node = next_node
 
-            # reconnect
-            tmp = group_prev.next
-            group_prev.next = prev
-            group_prev = tmp
+            # reconnect previous group to the reversed one
+            new_group_tail = prev_group_tail.next  # will become tail after reverse
+            prev_group_tail.next = reversed_head  # connect previous group to new head
+            prev_group_tail = new_group_tail  # move to next group
 
 
 @pytest.mark.parametrize("arr, k, expected", [
     ([1, 2, 3, 4, 5], 2, [2, 1, 4, 3, 5]),
+    ([1, 2, 3, 4, 5], 3, [3, 2, 1, 4, 5]),
 ])
 def test_reverse_nodes(arr, k, expected):
     head = build_linked_list(arr)
